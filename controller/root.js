8c3c5c553fd1e97dafd8badf7a2c5922f78edb13
Josimar Souza <contact.josimarsouza@gmail.com>
2021-12-16 13:41:01 -0300

    Criado controller para criar um novo usuário

--- controller/root.js
+++ controller/root.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const moviesRouter = require('./movies/MoviesRouter');
+const usersRouter = require('./users/UsersRouter');
 
 const root = express.Router({ mergeParams: true });
 
 moviesRouter(root);
+usersRouter(root);
 
 module.exports = (app) => {
   app.use(root);
