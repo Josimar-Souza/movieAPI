2178d289373398bb5b0560168eb6813dd243f4ad
Josimar Souza <contact.josimarsouza@gmail.com>
2021-12-17 11:06:35 -0300

    Adiciona o id do usuário ao filme criado

--- controller/movies/Create.js
+++ controller/movies/Create.js
@@ -3,7 +3,12 @@ const moviesService = require('../../service/movies');
 
 module.exports = async (req, res, next) => {
   try {
-    const newMovie = await moviesService.Create(req.body);
+    const movie = {
+      ...req.body,
+      userId: req.user._id,
+    };
+
+    const newMovie = await moviesService.Create(movie);
 
     return res.status(StatusCodes.CREATED).send(newMovie);
   } catch (error) {
