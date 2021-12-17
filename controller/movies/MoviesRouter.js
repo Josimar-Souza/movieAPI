141ed772cecbfcdb50b02ffca6a84ca9b54e3339
Josimar Souza <contact.josimarsouza@gmail.com>
2021-12-17 11:51:24 -0300

    Criado controller para remover um filme

--- controller/movies/MoviesRouter.js
+++ controller/movies/MoviesRouter.js
@@ -4,12 +4,14 @@ const Auth = require('../../middlewares/Auth');
 const Create = require('./Create');
 const GetALl = require('./GetAll');
 const GetByID = require('./GetByID');
+const Remove = require('./Remove');
 
 const router = express.Router({ mergeParams: true });
 
 router.get('/', GetALl);
 router.get('/:id', GetByID);
 router.post('/', Auth, CreateValidation, Create);
+router.delete('/:id', Auth, Remove);
 
 module.exports = (root) => {
   root.use('/movies', router);
