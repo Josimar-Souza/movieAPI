4341cd8d814dfc2c8e6311252f1f582ba69954a1
Josimar Souza <contact.josimarsouza@gmail.com>
2021-12-21 15:09:09 -0300

    Adiciona uma rota para retornar a imagem do filme

--- api/app.js
+++ api/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const { StatusCodes } = require('http-status-codes');
 const Error = require('../middlewares/Error');
@@ -6,8 +7,9 @@ const rootController = require('../controller/root');
 const app = express();
 
 app.use(express.json());
+app.use('/movies/images', express.static(path.resolve(__dirname, '../uploads')));
 
-app.get('/ping', (req, res) => res.status(StatusCodes.OK).send({ message: 'pong' }));
+app.get('/ping', (_req, res) => res.status(StatusCodes.OK).send({ message: 'pong' }));
 
 rootController(app);
 
